3c593cff25d8696c2dcea27ac3a9d422e3a4d392
Hiedayah <ct_norhidayah94@yahoo.com>
2018-10-05 18:18:51 +0800

    login

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,12 +1,12 @@
 class Expense < ApplicationRecord
 	belongs_to :claim
-	enum expense_type: [:travel, :food, :parking, :toll, :misc, :fixed_asset]
+	enum expense_type: [:travel_distance, :food, :parking, :toll, :misc, :fixed_asset]
 	mount_uploader :file, FileUploader
 
 	def price
 		case expense_type
-			when "travel"
-				Setting.v('travel_price').to_f * expense_value
+			when "travel_distance"
+				(Setting.v('travel_price').to_f * expense_value).round(2)
 			else
 				expense_value
 		end
--- app/views/expenses/_expense.json.jbuilder
+++ app/views/expenses/_expense.json.jbuilder
@@ -1,4 +1,5 @@
 Rails.logger.debug("MASUK JSON NIE")
 json.extract! expense, :id, :expense_date, :description, :expense_type, :expense_value, :file
+json.price expense.price
 json.file expense.file_identifier
 #json.url expense_url(expense, format: :json)
